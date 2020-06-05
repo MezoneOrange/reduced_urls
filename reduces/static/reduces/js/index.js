d1249b7793597f1b218a802c57e2199d6d288854
MezoneOrange <mezor07@mail.ru>
2020-06-05 22:42:50 +0500

    Animated logo

--- reduces/static/reduces/js/index.js
+++ reduces/static/reduces/js/index.js
@@ -3,5 +3,10 @@
         setTimeout(function() {
             $('.message').slideUp("slow");
         }, 7000);
+
+        $('#logo').on("mouseover", function() {
+            $('#logo img').delay(300).animate({rotate: '20deg'}, 500);
+        })
+
     });
 })(jQuery);
